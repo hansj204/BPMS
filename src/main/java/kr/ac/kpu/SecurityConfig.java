d1596845ded08a0b3e86848c103c71ce1845b7a6
Sujeong Han <42490329+hansj204@users.noreply.github.com>
2022-08-23 17:18:11 +0900

    보안 설정 수정

--- src/main/java/kr/ac/kpu/SecurityConfig.java
+++ src/main/java/kr/ac/kpu/SecurityConfig.java
@@ -1,5 +1,6 @@
 package kr.ac.kpu;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
